f25422b4098ccf037187be8c0f5722e38b19ef2e
Carlos Hernandez <khemtz@gmail.com>
2022-02-15 14:25:48 -0600

    se agregaron condiuciones if_else

--- imc.dart
+++ imc.dart
@@ -12,5 +12,37 @@ void main() {
 
   var imc = peso / (altura * altura);
 
-  print("tu IMC es" + imc.toString());
+  print("tu IMC es " + imc.toString());
+
+  var indice = 10;
+
+  if (imc > 40) {
+    print("Obesidad 3");
+  } else if (imc > 35) {
+    print("Obesidad 2");
+  } else if (imc > 30) {
+    print("Obesidad 1");
+  } else if (imc > 25) {
+    print("sobrepeso");
+  } else {
+    print("OK");
+  }
+
+  print("****************************");
+  if (imc > 40) {
+    print("Obesidad 3");
+  }
+
+  if ((imc > 35) && (imc < 39.9)) {
+    print("Obesidad 2");
+  }
+  /*
+  while (indice > 0) {
+    print("test" + indice.toString());
+    indice--;
+  }
+
+  for (var indice = 10; indice > 0; indice--) {
+    print("test" + indice.toString());
+  }*/
 }
