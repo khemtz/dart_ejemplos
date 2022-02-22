75738f718f8dc67a90ad51b4530e598930a18414
Carlos Hernandez <khemtz@gmail.com>
2022-02-22 14:09:28 -0600

    reto2

--- area_circulo.dart
+++ area_circulo.dart
@@ -6,6 +6,11 @@ void AreaCirculo(int r) {
   print("el area es:" + area.toString());
 }
 
+void AreaRectangulo(int b, int a) {
+  var area = b * a;
+  print("el area es:" + area.toString());
+}
+
 int LeerConsolaint(String msje) {
   stdout.write(msje);
   var readconsole = stdin.readLineSync();
@@ -14,7 +19,28 @@ int LeerConsolaint(String msje) {
 }
 
 void main() {
-  int radio = LeerConsolaint("cual es el radio:");
-  AreaCirculo(radio);
+  var respuesta = 9;
+
+  while (respuesta != 0) {
+    print("MENU");
+    print("1.- Circulo");
+    print("2.- Rectangulo");
+    print("3.- Triangulo");
+    print("4.- Cuadrado");
+    print("0.- Salir");
+    respuesta = LeerConsolaint("Elige una opcion:");
+
+    if (respuesta == 1) {
+      int radio = LeerConsolaint("cual es el radio:");
+      AreaCirculo(radio);
+    } else if (respuesta == 2) {
+      //print("calculo rectangulo");
+      int base = LeerConsolaint("cual es la base:");
+      int altura = LeerConsolaint("cual es la altura:");
+      AreaRectangulo(base, altura);
+    } else if (respuesta == 3) {
+      print("calculo triangulo");
+    }
+  }
   //print(radio.toString());
 }
